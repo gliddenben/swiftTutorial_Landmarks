bc9bd050c64579a25c48b708c6ac3da01ee1b3f1
Ben Glidden <gliddenben@gmail.com>
2024-03-30 19:16:43 -0400

    created the LandmarkRow struct/view

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -10,10 +10,10 @@ import SwiftUI
 import CoreLocation
 
 struct Landmark: Hashable, Codable {
-    var id: String
+    var id: Int
     var name: String
-    var state: String
     var park: String
+    var state: String
     var description: String
     
     private var imageName: String
---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,36 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Ben Glidden on 3/30/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image.resizable().frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview("Turtle Rock") {
+    LandmarkRow(landmark: landmarks[0])
+}
+
+#Preview("Salmon") {
+    LandmarkRow(landmark: landmarks[1])
+}
+
+#Preview("First Three") {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+        LandmarkRow(landmark: landmarks[2])
+    }
+}
